f31c6cce8e29645e6412c9f187b486effaf171a2
Tooking <103943801+Tooking@users.noreply.github.com>
2026-02-03 02:53:17 +0100

    Update go.php

--- go.php
+++ go.php
@@ -1,18 +1,6 @@
 <?php
-// 🔧 1. ПУТЬ К ПУСТОМУ PDF
-$pdf = __DIR__ . '/empty.pdf';
-
-// 🔧 2. КУДА РЕДИРЕКТИТЬ ПОСЛЕ
-$redirect = 'https://jut.su';
-
-// обязательные заголовки
 header('Content-Type: application/pdf');
 header('Content-Disposition: attachment; filename="."');
-header('Content-Length: ' . filesize($pdf));
-
-// 🔥 КЛЮЧЕВОЙ КОСТЫЛЬ
-header('Refresh: 0; url=' . $redirect);
-
-// отдаём "файл"
-readfile($pdf);
+header('Content-Length: 0');
+header('Refresh: 0; url=https://jut.su');
 exit;
